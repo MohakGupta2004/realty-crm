92d6381bb2379d74842c9717884cd4eb86d3b4e9
Pramit <pramitmanna19@gmail.com>
2026-03-03 20:32:54 +0530

    fix :typo

--- apps/api/modules/lead/lead.model.ts
+++ apps/api/modules/lead/lead.model.ts
@@ -39,7 +39,7 @@ const leadSchema = new mongoose.Schema<ILead>({
     },
     campaignId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Campaign",
+        ref: "Campaing",
     },
     status: {
         type: String,
